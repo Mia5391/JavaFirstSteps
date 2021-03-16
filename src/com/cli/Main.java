d970596b9c736466ebe309f29810bd9b7dad62cc
Mia <mariakrasteva5391@gmail.com>
2021-03-16 17:10:37 -0300

    implement arithmetic functionality

--- src/com/cli/Main.java
+++ src/com/cli/Main.java
@@ -4,10 +4,15 @@ import java.util.Arrays;
 import java.util.Date;
 
 public class Main {
+//constants are declared by putting final at the beginning (before the type of variable) and their names in uppercase (convention)
 
     public static void main(String[] args) {
-        manipulateArray();
+        arithmetic();
 
+    }
+    private static void arithmetic(){
+        double result = (double)10/(double)3;
+        System.out.println(result);
     }
 
     private static void manipulateArray() {
